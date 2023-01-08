f68d4e060e0bcb572a368dba745ee72484988e27
Anthony <76662184+FortniteYoutuberx344@users.noreply.github.com>
2023-01-07 22:53:58 -0800

    Update Othello.java

--- src/com/mrjaffesclass/othello/Othello.java
+++ src/com/mrjaffesclass/othello/Othello.java
@@ -16,11 +16,6 @@ public class Othello {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-/*
-    Nguyen player = new Nguyen(Constants.BLACK);
-    Board board = new Board();
-    player.getNextMove(board);
-*/
 
     Controller c = new Controller( 
         new Algorithm(Constants.BLACK), 
